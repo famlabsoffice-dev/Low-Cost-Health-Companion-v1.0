c2652fd1832fd98ce7c8658c081d4b5e0ace012b
famlabsoffice-dev <famlabsoffice@gmail.com>
2026-08-06 19:48:24 +0200

    Sprint 2: add secure media repository interface

---
+++ src/security/media/mediaRepository.ts
@@ -0,0 +1,17 @@
+import { EncryptedBlobStore, EncryptedBlobRecord } from './encryptedBlobStore';
+
+export class MediaRepository {
+  constructor(private readonly store = new EncryptedBlobStore()) {}
+
+  save(record: EncryptedBlobRecord) {
+    return this.store.save(record);
+  }
+
+  get(id: string) {
+    return this.store.get(id);
+  }
+
+  remove(id: string) {
+    return this.store.remove(id);
+  }
+}
