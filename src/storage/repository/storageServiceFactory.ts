f654d96715973e40ca4611c8448bb3601ee816d4
famlabsoffice-dev <famlabsoffice@gmail.com>
2026-08-06 22:48:24 +0200

    fix(storage): align repository factory schema integration

--- src/storage/repository/storageServiceFactory.ts
+++ src/storage/repository/storageServiceFactory.ts
@@ -1,9 +1,9 @@
 import { IndexedDbStorageRepository } from "./storageRepository";
 import { SecureStorage } from "../secureStorage";
-import { storageEntitySchema } from "../schemas/storageSchemas";
+import { versionedStorageSchema } from "../schemas/storageSchemas";
 
 export function createStorageService(namespace = "health_companion") {
-  const repository = new IndexedDbStorageRepository(storageEntitySchema);
+  const repository = new IndexedDbStorageRepository(versionedStorageSchema);
 
   return new SecureStorage(repository, namespace);
 }
