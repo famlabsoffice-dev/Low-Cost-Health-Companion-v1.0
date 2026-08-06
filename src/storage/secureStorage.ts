441a589c9a79d9af4be48fd4ee2e1f1e51ff53ee
famlabsoffice-dev <famlabsoffice@gmail.com>
2026-08-06 22:37:32 +0200

    feat(storage): integrate secure storage with repository layer

--- src/storage/secureStorage.ts
+++ src/storage/secureStorage.ts
@@ -1,7 +1,26 @@
+import { StorageRepository } from "./repository/storageRepository";
+
 export class SecureStorage {
   private readonly namespace: string;
+  private readonly repository: StorageRepository<unknown>;
 
-  constructor(namespace = "health_companion") {
+  constructor(repository: StorageRepository<unknown>, namespace = "health_companion") {
+    this.repository = repository;
     this.namespace = namespace;
+  }
+
+  async save(value: unknown) {
+    return this.repository.save({
+      ...(value as object),
+      namespace: this.namespace,
+    });
+  }
+
+  async get(id: string) {
+    return this.repository.get(id);
+  }
+
+  async remove(id: string) {
+    return this.repository.remove(id);
   }
 }
