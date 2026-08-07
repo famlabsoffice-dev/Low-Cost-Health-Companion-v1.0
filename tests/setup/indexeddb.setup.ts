e320abba25370abe755517599687fb5da41bb355
famlabsoffice-dev <famlabsoffice@gmail.com>
2026-08-07 07:00:21 +0200

    test(security): stabilize crypto tests for ci environment

--- tests/setup/indexeddb.setup.ts
+++ tests/setup/indexeddb.setup.ts
@@ -1,1 +1,9 @@
+import { webcrypto } from 'node:crypto';
 import 'fake-indexeddb/auto';
+
+if (!globalThis.crypto) {
+  Object.defineProperty(globalThis, 'crypto', {
+    value: webcrypto,
+    configurable: true,
+  });
+}
