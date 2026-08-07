626adc9a98ca916822d9b2f9fd7fe76dd8e3112e
famlabsoffice-dev <famlabsoffice@gmail.com>
2026-08-07 17:39:07 +0200

    feat: expose persistent crypto key provider

--- src/security/crypto/index.ts
+++ src/security/crypto/index.ts
@@ -1,5 +1,6 @@
 export * from './cryptoTypes';
 export * from './cryptoPipeline';
 export * from './cryptoKeyProvider';
+export * from './persistentCryptoKeyProvider';
 export * from './webCryptoEngine';
 export * from './securePayload';
