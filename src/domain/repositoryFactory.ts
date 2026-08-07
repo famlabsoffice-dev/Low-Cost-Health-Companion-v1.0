527792d66c9fa343c9535f4f0a3b5675f88db72d
famlabsoffice-dev <famlabsoffice@gmail.com>
2026-08-07 17:00:43 +0200

    refactor: enforce encrypted repository construction

--- src/domain/repositoryFactory.ts
+++ src/domain/repositoryFactory.ts
@@ -1,19 +1,10 @@
-import {
-  createCryptoPipeline,
-} from "../storage/repository/storageServiceFactory";
-import {
-  EncryptedRepository,
-} from "../security/storage/encryptedRepository";
-import {
-  MemorySecureStorage,
-} from "../security/storage/secureStorage";
-import {
-  HealthRecordRepository,
-} from "./healthRecordRepository";
+import { createCryptoPipeline } from "../storage/repository/storageServiceFactory";
+import { EncryptedRepository } from "../security/storage/encryptedRepository";
+import { MemorySecureStorage } from "../security/storage/secureStorage";
+import { HealthRecordRepository } from "./healthRecordRepository";
 
 export async function createHealthRecordRepository(): Promise<HealthRecordRepository> {
   const secureStorage = new MemorySecureStorage();
-
   const cryptoPipeline = await createCryptoPipeline();
 
   const encryptedRepository = new EncryptedRepository(
