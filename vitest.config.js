668242856e2d668785e2a349b4329e254438559c
famlabsoffice-dev <famlabsoffice@gmail.com>
2026-08-07 05:07:22 +0200

    test: fix vitest browser runtime discovery

--- vitest.config.js
+++ vitest.config.js
@@ -4,6 +4,8 @@ export default defineConfig({
   test: {
     environment: 'node',
     globals: true,
-    setupFiles: ['./tests/setup/indexeddb.setup.ts']
+    setupFiles: ['./tests/setup/indexeddb.setup.ts'],
+    include: ['tests/**/*.test.ts'],
+    exclude: ['tests/**/*.spec.js']
   }
 });
