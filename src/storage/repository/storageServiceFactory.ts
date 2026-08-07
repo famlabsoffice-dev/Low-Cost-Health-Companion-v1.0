317384001cf47e788cba953de3ae97b29c92c930
famlabsoffice-dev <famlabsoffice@gmail.com>
2026-08-07 15:26:01 +0200

    fix(storage): inject crypto pipeline into storage service factory

--- src/storage/repository/storageServiceFactory.ts
+++ src/storage/repository/storageServiceFactory.ts
@@ -1,9 +1,34 @@
+import { DefaultCryptoPipeline, type CryptoPipeline } from "../../security/crypto/cryptoPipeline";
+import { StaticCryptoKeyProvider } from "../../security/crypto/staticCryptoKeyProvider";
+import { WebCryptoEngine } from "../../security/crypto/webCryptoEngine";
 import { IndexedDbStorageRepository } from "./storageRepository";
 import { SecureStorage } from "../secureStorage";
 import { versionedStorageSchema } from "../schemas/storageSchemas";
 
-export function createStorageService(namespace = "health_companion") {
-  const repository = new IndexedDbStorageRepository(versionedStorageSchema);
+async function createCryptoPipeline(): Promise<CryptoPipeline> {
+  const key = await crypto.subtle.generateKey(
+    {
+      name: "AES-GCM",
+      length: 256,
+    },
+    true,
+    ["encrypt", "decrypt"],
+  );
+
+  const engine = new WebCryptoEngine(new StaticCryptoKeyProvider(key));
+
+  return new DefaultCryptoPipeline(engine);
+}
+
+export async function createStorageService(
+  namespace = "health_companion",
+  cryptoPipeline?: CryptoPipeline,
+) {
+  const pipeline = cryptoPipeline ?? (await createCryptoPipeline());
+  const repository = new IndexedDbStorageRepository(
+    versionedStorageSchema,
+    pipeline,
+  );
 
   return new SecureStorage(repository, namespace);
 }
