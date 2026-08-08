50f5533f77ddf32fc12aa9c58d2e73a9639b1e13
famlabsoffice-dev <famlabsoffice@gmail.com>
2026-08-08 20:57:40 +0200

    fix: remove duplicate secure storage factory

--- src/security/storage/storageServiceFactory.ts
+++ src/security/storage/storageServiceFactory.ts
@@ -1,25 +1,4 @@
-import { PersistentStorageCryptoKeyProvider } from '../crypto/persistentCryptoKeyProvider';
-import { WebCryptoEngine } from '../crypto/webCryptoEngine';
-import { DefaultCryptoPipeline, type CryptoPipeline } from '../crypto/cryptoPipeline';
-import { IndexedDbStorageRepository } from '../../storage/repository/storageRepository';
-import { SecureStorage } from './secureStorage';
-import { versionedStorageSchema } from '../../storage/schemas/storageSchemas';
-
-export async function createCryptoPipeline(): Promise<CryptoPipeline> {
-  const keyProvider = new PersistentStorageCryptoKeyProvider();
-  const engine = new WebCryptoEngine(keyProvider);
-  return new DefaultCryptoPipeline(engine);
-}
-
-export async function createStorageService(
-  namespace = 'health_companion',
-  cryptoPipeline?: CryptoPipeline,
-): Promise<SecureStorage> {
-  const pipeline = cryptoPipeline ?? await createCryptoPipeline();
-  const repository = new IndexedDbStorageRepository(
-    versionedStorageSchema,
-    pipeline,
-  );
-
-  return new SecureStorage(repository, namespace);
-}
+export {
+  createCryptoPipeline,
+  createStorageService,
+} from '../../storage/repository/storageServiceFactory';
