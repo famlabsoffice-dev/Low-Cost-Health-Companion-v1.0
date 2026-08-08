b37e0384415a8c057c9d76a60058f030d48a0d4c
famlabsoffice-dev <famlabsoffice@gmail.com>
2026-08-08 20:57:20 +0200

    refactor: unify secure storage factory

--- src/security/storage/storageServiceFactory.ts
+++ src/security/storage/storageServiceFactory.ts
@@ -1,9 +1,25 @@
-import { PersistentStorageCryptoKeyProvider } from './persistentStorageCryptoKeyProvider';
-import { IndexedDbSecureStorage } from './indexedDbSecureStorage';
+import { PersistentStorageCryptoKeyProvider } from '../crypto/persistentCryptoKeyProvider';
+import { WebCryptoEngine } from '../crypto/webCryptoEngine';
+import { DefaultCryptoPipeline, type CryptoPipeline } from '../crypto/cryptoPipeline';
+import { IndexedDbStorageRepository } from '../../storage/repository/storageRepository';
+import { SecureStorage } from './secureStorage';
+import { versionedStorageSchema } from '../../storage/schemas/storageSchemas';
 
-export function createStorageService() {
+export async function createCryptoPipeline(): Promise<CryptoPipeline> {
   const keyProvider = new PersistentStorageCryptoKeyProvider();
-  const storage = new IndexedDbSecureStorage();
+  const engine = new WebCryptoEngine(keyProvider);
+  return new DefaultCryptoPipeline(engine);
+}
 
-  return { storage, keyProvider };
+export async function createStorageService(
+  namespace = 'health_companion',
+  cryptoPipeline?: CryptoPipeline,
+): Promise<SecureStorage> {
+  const pipeline = cryptoPipeline ?? await createCryptoPipeline();
+  const repository = new IndexedDbStorageRepository(
+    versionedStorageSchema,
+    pipeline,
+  );
+
+  return new SecureStorage(repository, namespace);
 }
