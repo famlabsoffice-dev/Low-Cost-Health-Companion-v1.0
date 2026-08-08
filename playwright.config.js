51bfdeb410ce8eda18caed8715597cad321f5d2b
famlabsoffice-dev <famlabsoffice@gmail.com>
2026-08-08 18:31:23 +0200

    fix: isolate playwright test discovery from vitest specs

--- playwright.config.js
+++ playwright.config.js
@@ -4,6 +4,7 @@ const isTermux = Boolean(process.env.TERMUX_VERSION || process.env.ANDROID_ROOT 
 
 export default defineConfig({
   testDir: './tests',
+  testMatch: ['**/*.playwright.spec.js', '**/*.e2e.spec.js', '**/*.browser.spec.js'],
   timeout: 60000,
   reporter: [['list']],
   webServer: isTermux ? undefined : { command: 'node ./scripts/browser-test-server.mjs', url: 'http://127.0.0.1:4173', reuseExistingServer: true },
