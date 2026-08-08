4752d51bcf27405bd2c02623113ba52c21649b05
famlabsoffice-dev <famlabsoffice@gmail.com>
2026-08-08 18:22:46 +0200

    feat: expose persistent crypto key recovery operations

--- src/security/crypto/persistentCryptoKeyProvider.ts
+++ src/security/crypto/persistentCryptoKeyProvider.ts
@@ -4,9 +4,22 @@ import { PersistentCryptoKeyProvider } from '../keys/persistentCryptoKeyProvider
 export class PersistentStorageCryptoKeyProvider implements CryptoKeyProvider {
   constructor(
     private readonly provider = new PersistentCryptoKeyProvider(),
+    private readonly keyId = 'device-root-key',
   ) {}
 
   async getKey(): Promise<CryptoKey> {
-    return this.provider.getOrCreate();
+    return this.provider.getOrCreate(this.keyId);
+  }
+
+  async rotate(): Promise<CryptoKey> {
+    return this.provider.rotate(this.keyId);
+  }
+
+  async exportKey(): Promise<JsonWebKey> {
+    return this.provider.exportKey(this.keyId);
+  }
+
+  async remove(): Promise<void> {
+    await this.provider.remove(this.keyId);
   }
 }
