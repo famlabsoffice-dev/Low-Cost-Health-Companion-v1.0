47a7c473971d7cf27100707aac2a230aa5d3307b
famlabsoffice-dev <famlabsoffice@gmail.com>
2026-08-09 18:56:05 +0200

    refactor(security): consolidate production crypto exports

--- src/security/crypto/index.ts
+++ src/security/crypto/index.ts
@@ -1,6 +1,5 @@
 export * from './cryptoTypes';
 export * from './cryptoPipeline';
-export * from './cryptoKeyProvider';
 export * from './persistentCryptoKeyProvider';
 export * from './webCryptoEngine';
 export * from './securePayload';
