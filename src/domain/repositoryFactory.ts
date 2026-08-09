3a1dcb07bc3001dfff29a903ea0d928e2c15c227
famlabsoffice-dev <famlabsoffice@gmail.com>
2026-08-09 14:38:17 +0200

    feat: wire health repository to IndexedDB storage

--- src/domain/repositoryFactory.ts
+++ src/domain/repositoryFactory.ts
@@ -1,18 +1,14 @@
-import { createCryptoPipeline } from "../storage/repository/storageServiceFactory";
+import { createCryptoPipeline, createStorageService } from "../storage/repository/storageServiceFactory";
 import { EncryptedRepository } from "../security/storage/encryptedRepository";
-import { MemorySecureStorage } from "../security/storage/secureStorage";
 import { HealthRecordRepository } from "./healthRecordRepository";
 
 export async function createHealthRecordRepository(): Promise<HealthRecordRepository> {
-  const secureStorage = new MemorySecureStorage();
   const cryptoPipeline = await createCryptoPipeline();
-
+  const secureStorage = await createStorageService("health_records", cryptoPipeline);
   const encryptedRepository = new EncryptedRepository(
     secureStorage,
     cryptoPipeline,
   );
 
-  return new HealthRecordRepository(
-    encryptedRepository,
-  );
+  return new HealthRecordRepository(encryptedRepository);
 }
