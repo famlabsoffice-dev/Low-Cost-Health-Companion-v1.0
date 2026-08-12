c11406b8079267f8ffdc467d869653dee71fda9c
famlabsoffice-dev <famlabsoffice@gmail.com>
2026-08-12 16:18:31 +0200

    feat(input): expose health input service factory

--- src/domain/repositoryFactory.ts
+++ src/domain/repositoryFactory.ts
@@ -1,4 +1,5 @@
 import { createHealthRecordStorageRepository } from "../storage/repository/storageServiceFactory";
+import { HealthInputService } from "../input/healthInputService";
 import { HealthTimelineRepository } from "../timeline/healthTimelineRepository";
 import { HealthRecordRepository } from "./healthRecordRepository";
 
@@ -9,4 +10,8 @@ export async function createHealthRecordRepository(): Promise<HealthRecordReposi
 
 export async function createHealthTimelineRepository(): Promise<HealthTimelineRepository> {
   return new HealthTimelineRepository(await createHealthRecordRepository());
+}
+
+export async function createHealthInputService(): Promise<HealthInputService> {
+  return new HealthInputService(await createHealthTimelineRepository());
 }
