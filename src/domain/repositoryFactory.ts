81691ddd1835a9fc28f58120649c42db7f2c497b
famlabsoffice-dev <famlabsoffice@gmail.com>
2026-08-12 16:14:39 +0200

    feat(timeline): expose timeline repository factory

--- src/domain/repositoryFactory.ts
+++ src/domain/repositoryFactory.ts
@@ -1,7 +1,12 @@
 import { createHealthRecordStorageRepository } from "../storage/repository/storageServiceFactory";
+import { HealthTimelineRepository } from "../timeline/healthTimelineRepository";
 import { HealthRecordRepository } from "./healthRecordRepository";
 
 export async function createHealthRecordRepository(): Promise<HealthRecordRepository> {
   const repository = await createHealthRecordStorageRepository();
   return new HealthRecordRepository(repository);
+}
+
+export async function createHealthTimelineRepository(): Promise<HealthTimelineRepository> {
+  return new HealthTimelineRepository(await createHealthRecordRepository());
 }
